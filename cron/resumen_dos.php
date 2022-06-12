e2c58de80be3480c532f4d4dd7920f9718f3bfdb
Edgar-sto <72636802+Edgar-sto@users.noreply.github.com>
2022-06-11 22:32:10 -0500

    Update resumen_dos.php

--- cron/resumen_dos.php
+++ cron/resumen_dos.php
@@ -43,7 +43,7 @@ class sucursal
     }
 }
 date_default_timezone_set('America/Mexico_City');
-$date="2022-05-05";
+$date="2022-05-24";
 // $date = new DateTime();
 // $date->sub(new DateInterval('P1D'));
 // $date     =   $date->format('Y-m-d'); //Datos a guardar date('Y-m-d')
@@ -52,100 +52,116 @@ if ($date) {
 } else { // format failed
     echo "Unknown Time";
 }
+
 $conexion = conexion_fedora('telefonia', '10.9.2.234','3dg4rm4n','secretosdenegus');
-$prefijos    =   array("15","777","11", "999","28","444","14","555");
+$prefijos    =   array("15','777"/*,"11','999","28','444","14','555"*/);
 //$conexionlocal = conexion_db('telefonia', 'localhost');
 echo "Busqueda del dia $date ";
 echo "\n";
-$servidores = array(/*"5","6","8", "9", "11", "15","22", "27", "28", "29", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", */"48"/*, "201"*/);
+
+$insert_inicio="INSERT INTO `control_log_scripts` (`id_log`, `name_script`,`estatus`, `fecha`, `fecha_registro`)
+VALUES (NULL, 'Total Resumen', 'Inicio', '{$date}', current_timestamp());";
+
+if ($insert_inicio =   mysqli_query($conexion, $insert_inicio)) {
+    echo " |-Log de inicio Guardado exitosamente";
+    echo "\n";
+} else {
+    echo " |-No se pudo guardar log";
+    echo "\n";
+}
+
+
+
+
+$servidores = array("5","6","8", "9", "11", "15","22", "27", "28", "29", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48","57","60", "201");
 $tamanio_servidores = count($servidores);
 for ($x = 0; $x < $tamanio_servidores; $x++) {
     $reporte = $servidores[$x]; //Dato a guardar
-    echo "Reporte $reporte \n";
+    echo "|----Reporte $reporte \n";
     echo "\n";
-    $obtener_consumo_resumen_dos = "SELECT DISTINCT a.prefijo, a.campania, a.grupo, SUM(consumo) AS Total,
-    (CASE WHEN tipo = 'movil' THEN 'Movil'
-      WHEN tipo = 'drop_movil'  THEN 'Drop Movil'
-      WHEN tipo = 'buzon_movil'  THEN 'Buzon Movil'
-      WHEN tipo = 'fijo'  THEN 'Fijo'
-      WHEN tipo = 'drop_fijo'  THEN 'Drop Fijo'
-      WHEN tipo = 'buzon_fijo'  THEN 'Buzon Fijo'
-      END ) AS Tipo
-    FROM reporte_telefonia a
-    WHERE prefijo IN ('14','555')
-      AND fecha_inicio>='{$date} 00:00:00'
-      AND fecha_termino<='{$date} 23:59:59'
-      AND reporte='10.9.2.{$reporte}'
-      GROUP BY prefijo, campania, grupo, tipo,reporte
-    ORDER BY prefijo,campania,grupo ASC;";
-    $answer_resumen_dos = $conexion->query($obtener_consumo_resumen_dos);
-    while ($row = $answer_resumen_dos->fetch_object()) {
-        $row->prefijo;
-        $row->campania;
-        $row->grupo;
-        $row->Total;
-        $row->Tipo;
+    foreach ($prefijos as $value) {
+        
+        $obtener_consumo_resumen_dos = "SELECT DISTINCT a.prefijo, a.campania, a.grupo, SUM(consumo) AS Total,
+        (CASE WHEN tipo = 'movil' THEN 'Movil'
+        WHEN tipo = 'drop_movil'  THEN 'Drop Movil'
+        WHEN tipo = 'buzon_movil'  THEN 'Buzon Movil'
+        WHEN tipo = 'fijo'  THEN 'Fijo'
+        WHEN tipo = 'drop_fijo'  THEN 'Drop Fijo'
+        WHEN tipo = 'buzon_fijo'  THEN 'Buzon Fijo'
+        END ) AS Tipo
+        FROM reporte_telefonia a
+        WHERE prefijo IN ('{$value}')
+        AND fecha_inicio>='{$date} 00:00:00'
+        AND fecha_termino<='{$date} 23:59:59'
+        AND reporte='10.9.2.{$reporte}'
+        GROUP BY prefijo, campania, grupo, tipo,reporte
+        ORDER BY prefijo,campania,grupo ASC;";
+        $answer_resumen_dos = $conexion->query($obtener_consumo_resumen_dos);
+        while ($row = $answer_resumen_dos->fetch_object()) {
+            $row->prefijo;
+            $row->campania;
+            $row->grupo;
+            $row->Total;
+            $row->Tipo;
 
-        switch ($row->Tipo) {
-          case 'Movil':
-              $group = new sucursal($conexion,$row->grupo);
-              $y = $group->obtSucursal();
-              
-              break;
+            switch ($row->Tipo) {
+                case 'Movil':
+                $group = new sucursal($conexion,$row->grupo);
+                $y = $group->obtSucursal();
+                
+                break;
 
-          case 'Fijo':
-              $group = new sucursal($conexion,$row->grupo);
-              $y=$group->obtSucursal();
-              // foreach ($group->obtSucursal() as $sucursalValue) {
-              //   $y = $sucursalValue." ";
-              // }
-              break;
+                case 'Fijo':
+                $group = new sucursal($conexion,$row->grupo);
+                $y=$group->obtSucursal();
+                // foreach ($group->obtSucursal() as $sucursalValue) {
+                //   $y = $sucursalValue." ";
+                // }
+                break;
 
-          case 'Drop Movil':
-            $group = new sucursal($conexion,$reporte);
-              $w = $group->obtSucursalVici();
-              //var_dump($w);
-              $y = $w . " " . "DROP MOVIL";
-              // foreach ($group->obtSucursal() as $sucursalValue) {
-              //   $x = $sucursalValue." ";
-              //   $y = $x ." DROP MOVIL";
-              // }
-              //$x = "DROP MOVIL";
-              break;
+                case 'Drop Movil':
+                $y = "DROP MOVIL";
+                break;
 
-          case 'Drop Fijo':
-              $y = "DROP FIJO";
-              break;
-          
-          case 'Buzon Movil':
-              $y = "BUZON MOVIL";
-              break;
+                case 'Drop Fijo':
+                $y = "DROP FIJO";
+                break;
+            
+                case 'Buzon Movil':
+                $y = "BUZON MOVIL";
+                break;
 
-          case 'Buzon Fijo':
-              $y = "BUZON FIJO";
-              break;
-      }
+                case 'Buzon Fijo':
+                $y = "BUZON FIJO";
+                break;
+            }
 
-        echo "10.9.2.". $reporte ."       ".  $row->prefijo ."       ".  $row->campania  ."       ". $y ."   ". $row->grupo ."       ". $row->Total ."       ". $row->Tipo;
-        echo "\n";
+            //echo "10.9.2.". {$reporte ."  ".  $row->prefijo ."  ".  $row->campania  ."  ". $y ."  ". $row->grupo ."  ". $row->Total ."       ". $row->Tipo;
+            //echo "\n";
 
-
-
-
+            $insertar   =   "INSERT INTO `reporte_telefonia_resumendos` (`id`, `reporte`, `prefijo`, `id_campania`, `sucursal_cliente_campania`, `grupo`, `consumo`, `tipo`, `fecha_inicio`, `fecha_termino`, `fecha_registro`)
+            VALUES (NULL, '10.9.2.{$reporte}', '{$row->prefijo}', '{$row->campania}', '{$y}', '{$row->grupo}', '{$row->Total}', '{$row->Tipo}', '{$date} 00:00:00', '{$date} 23:59:59', CURRENT_TIMESTAMP);";
+            if ($insertar_ = mysqli_query($conexion, $insertar)) {
+                echo "|------Record saved successfully";
+                echo "\n";
+            } else {
+                echo "|------El registro de la fecha {$date} del Reporte {$reporte} no se guardo en la Base";
+                echo "\n";
+            }
+        }
     }
-    //     if (empty($campaing) || empty($group)) {
-    //         //espacio vacio no hace nada
-    //     } else {
-    //         //echo "$campaing" . "  " . "$group";
-    //         //echo "\n";
-    //         $insertar   =   "INSERT INTO `consumo_grupos_raptor` (`id_grupos_raptor`, `reporte`, `campania`, `grupos`, `fecha_inicio`, `fecha_fin`)
-    //                         VALUES (NULL, '10.9.2.{$reporte}', '{$campaing}', '{$group}', '{$date} 00:00:00', '{$date} 23:59:59')";
-    //         if ($insertar_ = mysqli_query($conexionlocal, $insertar)) {
-    //             echo "Record saved successfully";
-    //             echo "\n";
-    //         } else {
-    //             echo "El registro de la fecha {$date} del Reporte {$reporte} no se guardo en la Base";
-    //             echo "\n";
-    //         }
-    //     }
+}
+
+
+
+
+$insert_fin="INSERT INTO `control_log_scripts` (`id_log`, `name_script`,`estatus`, `fecha`, `fecha_registro`)
+VALUES (NULL, 'Total Resumen', 'Termino', '{$date}', current_timestamp());";
+
+if ($insert_fin =   mysqli_query($conexion, $insert_fin)) {
+    echo " |-Log de finalizacion Guardado exitosamente.";
+    echo "\n";
+} else {
+    echo " |-No se pudo guardar log de finilización.";
+    echo "\n";
 }
